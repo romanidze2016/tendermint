837379170baafa642ab22d44071bb23ad8413b6d
Roman Pazinich <romanpazinich@vlan-2648-10-16-198-114.student.wireless.sydney.edu.au>
2018-02-22 12:13:24 +1100

    minor change

--- consensus/state.go
+++ consensus/state.go
@@ -655,7 +655,7 @@ func (cs *ConsensusState) handleTxsAvailable(height int64) {
 	cs.mtx.Lock()
 	defer cs.mtx.Unlock()
 	// we only need to do this for round 0
-	cs.enterPropose(height, 0)
+	//cs.enterPropose(height, 0)
 	cs.RB_broadcast(height, 0)
 }
 
